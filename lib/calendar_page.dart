26f30cb41c22efaabfc2ca020f1e47b65a270ab0
Marc-antoine simon <marc-antoine.simon3@orange.fr>
2022-03-24 13:37:42 +0100

    add project

---
+++ lib/calendar_page.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+DateTime kNow = DateTime.now();
+DateTime kFirstDay = DateTime(kNow.year, kNow.month - 3, kNow.day);
+DateTime kLastDay = DateTime(kNow.year, kNow.month + 3, kNow.day);
+
+const d_green = Color(0xFF54D3C2);
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppBar(),
+      body: Column(
+        children:  const [
+          PeriodSection(),
+          CalendarRange(),
+          ValidateBookingSection(),
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.grey[800],
+          size: 20,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class PeriodSection extends StatelessWidget {
+  const PeriodSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Départ',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 4),
+                const Text(
+                  'Month 12 Dec',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 60,
+              width: 1,
+              color: Colors.grey[350],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Return',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 4),
+                const Text(
+                  'Tues 22 Dec',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Divider(
+          color: Colors.grey,
+          height: 1,
+        ),
+      ],
+    );
+  }
+}
+
+class CalendarRange extends StatefulWidget {
+  const CalendarRange({Key? key}) : super(key: key);
+
+  @override
+  _CalendarRangeState createState() => _CalendarRangeState();
+}
+
+class _CalendarRangeState extends State<CalendarRange> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOn;
+
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      color: Colors.grey[10],
+      child: Expanded(
+        child: TableCalendar(
+          lastDay: kLastDay,
+          focusedDay: _focusedDay,
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          rangeStartDay: _rangeStart,
+          rangeEndDay: _rangeEnd,
+          calendarFormat: _calendarFormat,
+          headerStyle: const HeaderStyle(
+            titleCentered: true,
+            formatButtonVisible: false,
+            titleTextStyle: TextStyle(fontSize: 18),
+          ),
+          calendarStyle: const CalendarStyle(
+            isTodayHighlighted: false,
+            rangeHighlightColor: d_green,
+            rangeStartDecoration: BoxDecoration(
+              color: d_green,
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(
+                  color: Colors.white,
+                  width: 3,
+                  style: BorderStyle.solid,
+                ),
+              ),
+            ),
+            rangeEndDecoration: BoxDecoration(
+              color: d_green,
+              shape: BoxShape.circle,
+              border: Border.fromBorderSide(
+                BorderSide(
+                  color: Colors.white,
+                  width: 3,
+                  style: BorderStyle.solid,
+                ),
+              ), 
+            ),
+          ),
+          firstDay: kFirstDay,
+          rangeSelectionMode: _rangeSelectionMode,
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+                _rangeStart = null;
+                _rangeEnd = null;
+                _rangeSelectionMode = RangeSelectionMode.toggledOff;
+              });
+            }
+          },
+          onRangeSelected: (start, end, focusedDay) {
+            setState(() {
+              _selectedDay = null;
+              _focusedDay = focusedDay;
+              _rangeStart = start;
+              _rangeEnd = end;
+              _rangeSelectionMode = RangeSelectionMode.toggledOn;
+            });
+          },
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ValidateBookingSection extends StatelessWidget {
+  final selectedRadio = 1;
+
+  const ValidateBookingSection({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RadioListTile(
+          value: selectedRadio,
+          activeColor: d_green,
+          groupValue: selectedRadio,
+          selected: true,
+          title: const Text(
+            'Flexible with dates',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0,
+            ),
+          ),
+          onChanged: null,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          width: double.infinity,
+          child: ElevatedButton(
+            child: const Text(
+              'Apply',
+              style: TextStyle(fontSize: 17),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: d_green,
+              padding: const EdgeInsets.all(15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+            onPressed: () {
+              print('Apply Booking');
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,347 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'calendar_page.dart';
+import 'hotel_card.dart';
+
+const d_green = Color(0xFF54D3C2);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hotels Booking',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const MyAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SearchSection(),
+            HotelSection()
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavBarSection(),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({Key? key}) : super(key: key);
+
+  @override
+  Size get preferredSize => const Size.fromHeight(50);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.grey[800],
+          size: 20,
+        ),
+        onPressed: null,
+      ),
+      centerTitle: true,
+      title: Text(
+        'Explore',
+        style: GoogleFonts.nunito(
+          color: Colors.black,
+          fontSize: 22,
+          fontWeight: FontWeight.w800
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.favorite_outline_rounded,
+            color: Colors.grey[800],
+            size: 20
+          ),
+          onPressed: null,
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.place,
+            color: Colors.grey[800],
+            size: 20
+          ),
+          onPressed: null,
+        ),
+      ],
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  const SearchSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.fromLTRB(10, 25, 10, 10),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade300,
+                        blurRadius: 4,
+                        offset: const Offset(0, 3)
+                      ),
+                    ],
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'London',
+                      contentPadding: EdgeInsets.all(10),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 4,
+                      offset: const Offset(0, 4),
+                    ),
+                  ],
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const CalendarPage();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 26,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    shape: const CircleBorder(),
+                    shadowColor: Colors.white,
+                    primary: d_green,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Choose date',
+                      style: GoogleFonts.nunito(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      '12 Dec - 22 Dec',
+                      style: GoogleFonts.nunito(
+                        color: Colors.black,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Number of Rooms',
+                      style: GoogleFonts.nunito(
+                        color: Colors.grey,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      '1 Room - 2 Adults',
+                      style: GoogleFonts.nunito(
+                        color: Colors.black,
+                        fontSize: 17,
+                      ),
+                    ), 
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HotelSection extends StatelessWidget {
+  HotelSection({Key? key}) : super(key: key);
+  final List hotelList = [
+    {
+      'title': 'Grand Royl Hotel',
+      'place': 'wembley, London',
+      'distance': 2,
+      'review': 36,
+      'picture': 'assets/images/hotel1.jpeg',
+      'price': '180',
+    },
+    {
+      'title': 'Queen Hotel',
+      'place': 'wembley, London',
+      'distance': 3,
+      'review': 13,
+      'picture': 'assets/images/hotel2.jpeg',
+      'price': '220',
+    },
+    {
+      'title': 'Grand Mal Hotel',
+      'place': 'wembley, London',
+      'distance': 6,
+      'review': 88,
+      'picture': 'assets/images/hotel3.jpeg',
+      'price': '400',
+    },
+    {
+      'title': 'Hilton',
+      'place': 'wembley, London',
+      'distance': 11,
+      'review': 34,
+      'picture': 'assets/images/hotel4.jpeg',
+      'price': '910',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '550 hotels founds',
+                  style: GoogleFonts.nunito(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Filters',
+                      style: GoogleFonts.nunito(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const IconButton(
+                      icon: Icon(
+                        Icons.filter_list_outlined,
+                        color: d_green,
+                        size: 25,
+                      ),
+                      onPressed: null,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: hotelList.map((hotel) {
+              return HotelCard(hotel);
+            }).toList(),
+          ),
+        ],
+      ),
+    );    
+  }
+}
+
+class BottomNavBarSection extends StatelessWidget {
+  const BottomNavBarSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.grey[600],
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.search,
+            color: d_green,
+          ),
+          label: 'Explore',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.favorite_outline_rounded,
+            color: d_green,
+          ),
+          label: 'Tips',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+            color: d_green,
+          ),
+          label: 'Profile',
+        ),
+      ],
+    );
+  }
+}
